f069f1d93fdbd129fff301b2d59e83535366b615
AGSTRANGER <hannibaletunisien@gmail.com>
2020-01-26 16:23:04 +0100

    Succesfully connected to mongoDB Atlas database server

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Ahmed:TMOW5F6iq9Mgtbp5@cluster0-shqnc.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
-  res.send("fyck");
+  res.send("Hello");
 });
+
 //process.env.Port is for Heroku
 const port = process.env.Port || 5000;
 // `` ES6 Template literal is used so that we can put a variable inside the String
