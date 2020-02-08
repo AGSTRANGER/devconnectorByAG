f63858a700ec5a559b1492e4189adff148206b72
AGSTRANGER <hannibaletunisien@gmail.com>
2020-02-08 10:05:35 +0100

    Added routing with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
+// TODO: Why is component imported inside of brackets
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing}></Route>
+          <div className="container">
+            {/*  We will create a container that wraps everything except the landing page
+             *  because we want the landing page to span across the whole screen*/}
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
